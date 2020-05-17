92474a078caf91fb03ae1fdcb2428badb6d3ab6e
Kanmii <maneptha@gmail.com>
2020-05-17 11:12:20 +0200

    feature: Add missing fields to artist model

--- artist/models.py
+++ artist/models.py
@@ -5,12 +5,15 @@ class Artist(db.Model):
     __tablename__ = "artist"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
